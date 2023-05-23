c9f8fe9366115b5fc46fe6d9b3b082859390ee2f
IulianaJ <iuliana.jaba@gmail.com>
2023-05-23 14:03:48 +0300

    Add first tests

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,17 @@
-describe("template spec", () => {
-  it("passes", () => {
-    cy.visit("//localhost:3000/");
+describe("test login functionality", function () {
+  beforeEach(function () {
+    cy.visit("//localhost:3000/")
+      .get('[data-cy="email"]')
+      .type("iuliana.jaba@thinslices.com")
+      .get('[data-cy="password"]')
+      .type("WorkingHours!")
+      .get('[data-cy="loginButton"]')
+      .click();
   });
+  it("should be logged in the app", function () {});
+  it("should display a table and a button", function () {});
+  it("should contain an week label", function () {});
+  it("should display a modal when the day cell is clicked", function () {});
+  it("should close the modal when the button is clicked", function () {});
+  it("should calculate the average weekly hours when the button is clicked", function () {});
 });
